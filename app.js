0e1ab3de34574d1fc3bd0c1d385df093f225f4b0
Eric Shin <eric@therocketfarm.com>
2020-02-05 16:31:39 -0800

    building structure

---
+++ app.js
@@ -0,0 +1,12 @@
+class KeyCommandManager(){
+	constructor(){
+		this.keyStream = '';
+		this.commands = {};
+		this.registerKeyCommand = this.registerKeyCommand.bind(this);
+	}
+
+	registerKeyCommand(command, callback){
+		
+	}
+
+}
